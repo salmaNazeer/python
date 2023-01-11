245bef91bf149dc48a257d5cb2b196bbca8ecf69
salmaNazeer <119401688+salmaNazeer@users.noreply.github.com>
2023-01-11 14:48:00 +0530

    Create positivenum.py

---
+++ positivenum.py
@@ -0,0 +1,5 @@
+list_1=[-150,-24,98,63,0,-33,11]
+print("The given list is :",list_1)
+print("positive numbers from list of integers using list comprehension :")
+list_1=[i for i in list_1 if i>=0]
+print(list_1)
